b934da4909ddd9cb39e44847b32384f037c44a52
Nathalie Hippchen <n_hippchen18@cs.uni-kl.de>
2020-05-15 15:39:07 +0200

    Finale Version Aufgabe 2

--- MP1/src/main/java/de/tukl/programmierpraktikum2020/mp1/ArrayMap.java
+++ MP1/src/main/java/de/tukl/programmierpraktikum2020/mp1/ArrayMap.java
@@ -2,16 +2,14 @@ package de.tukl.programmierpraktikum2020.mp1;
 
 public class ArrayMap implements Map<String, Integer> {
 
-    ArrayPair arr[] = new ArrayPair[100];
+    ArrayPair[] arr= new ArrayPair[100]; //erstelle Array mit Länge 100
 
     @Override
     public Integer get(String key) {
         for (int i = 0; i < arr.length; i++) { //wenn key gefunden wird, wird value ausgegeben
-            if (arr[i] != null) {
+            if (arr[i] != null) { //bei Einträgen, die nicht null sind
                 if (arr[i].getFirst().equals(key)) {
-                    int result;
-                    result = arr[i].getSecond();
-                    return result;
+                    return arr[i].getSecond();
                 }
             }
         }
@@ -22,7 +20,7 @@ public class ArrayMap implements Map<String, Integer> {
     public void put(String key, Integer value) {
         boolean eingefuegt = false;
         for (int i = 0; i < arr.length; i++) { //wenn key gefunden, wird value neu gesetzt
-            if (arr[i] != null) {
+            if (arr[i] != null) {  //bei Einträgen, die nicht null sind
                 if (arr[i].getFirst().equals(key)) {
                     remove(key);
                     arr[i] = new ArrayPair(key, value);
@@ -32,7 +30,7 @@ public class ArrayMap implements Map<String, Integer> {
             }
         }
         if (!eingefuegt) {
-            for (int i = 0; i < arr.length; i++) { //wenn key nicht gefunden, aber freie Einträge vorhanden, werden key und value gesetzt
+            for (int i = 0; i < arr.length; i++) { //wenn key nicht gefunden, aber freie Einträge vorhanden, wird neues ArrayPair-Element mit key und value erzeugt
                 if (arr[i] == null) {
                     arr[i] = new ArrayPair(key, value);
                     eingefuegt = true;
@@ -40,40 +38,29 @@ public class ArrayMap implements Map<String, Integer> {
                 }
             }
         }
-        if (!eingefuegt) {
-            //key nicht gefunden, keine freien Einträge -> Array muss neu erstellt und kopiert werden
-            ArrayPair oldarr[] = new ArrayPair[arr.length]; //neues "altes" Array wird erstellt
+        if (!eingefuegt) { //key nicht gefunden, keine freien Einträge -> Array muss neu erstellt und kopiert werden
+            ArrayPair[] oldarr = new ArrayPair[arr.length]; //neues "altes" Array wird erstellt
             for (int i = 0; i < arr.length; i++) { //Einträge werden kopiert
                 oldarr[i] = arr[i];
             }
 
-            ArrayPair arr[] = new ArrayPair[2 * oldarr.length]; //Array mit doppelter Länge wird erstellt
+            ArrayPair[] arr = new ArrayPair[2 * oldarr.length]; //Array mit doppelter Länge wird erstellt
             for (int i = 0; i < oldarr.length; i++) { //Einträge werden kopiert, damit hat neues Array mit doppelter Länge gleichen Namen wie ursprüngliches Array hat
                 arr[i] = oldarr[i];
             }
 
-            for (int i = oldarr.length; i < arr.length; i++) { //beginne bei oldarr.length, da Einträge davor bekanntermaßen NICHT null sind
-                if (arr[i] != null) {
-                    if (arr[i].getFirst() == null) {
-                        arr[i].setFirst(key); //setze key
-                        arr[i].setSecond(value); //setze value
-                        break;
-                    }
-                }
-            }
+            arr[oldarr.length] = new ArrayPair(key, value); //bei erstem Eintrag, der null ist (d.h. hinter "altem" Array), wird neues ArrayPair mit key und value erzeugt
         }
-        //  this.arr = arr; //damit auch außerhalb dieser Methode geändert
     }
 
 
     @Override
     public void remove(String key) {
-        for (int i = 0; i < arr.length; i++) { //wenn key gefunden, werden beide Werte auf null gesetzt
-            if (arr[i] != null) {
+        for (int i = 0; i < arr.length; i++) { //wenn key gefunden, wird Eintrag null gesetzt
+            if (arr[i] != null) {  //bei Einträgen, die nicht null sind
                 if (arr[i].getFirst().equals(key)) {
                     arr[i] = null;
                     break;
-
                 }
             }
         }
@@ -83,12 +70,9 @@ public class ArrayMap implements Map<String, Integer> {
     public int size() {
         int eintraege = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != null) {
-                if (arr[i].getFirst() != null) { //wenn Eintrag nicht null ist, wird Counter erhöht
-                    eintraege += 1;
-                }
+            if (arr[i] != null) {//wenn Eintrag nicht null ist, wird Counter erhöht
+                eintraege += 1;
             }
-
         }
         return eintraege; //gibt  Anzahl der Einträge zurück
     }
@@ -103,8 +87,7 @@ public class ArrayMap implements Map<String, Integer> {
         } else {
             int j = 0;
             for (int i = 0; i < arr.length; i++) {
-                if (arr[i] != null) {
-                    //alle Einträge des Arrays, die nicht null sind
+                if (arr[i] != null) {  //alle Einträge des Arrays, die nicht null sind
                     array[j] = arr[i].getFirst(); //werden in das eingegebene Array geschrieben (ohne Lücken)
                     j += 1;
                 }
