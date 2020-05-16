84804770124804c8de843c01a75786381d91f9fe
Manuela <mkawoung@gmail.com>
2020-05-16 23:07:16 +0200

    Aufgabe4 test version

--- MP1/src/main/java/de/tukl/programmierpraktikum2020/mp1/BibleAnalyzer.java
+++ MP1/src/main/java/de/tukl/programmierpraktikum2020/mp1/BibleAnalyzer.java
@@ -1,56 +1,49 @@
 package de.tukl.programmierpraktikum2020.mp1;
 
 import java.util.Comparator;
-import java.util.List;
 
-public class  BibleAnalyzer {
-
+public class BibleAnalyzer {
     public static void countWords(Map<String, Integer> counts) {
         for (String word : Util.getBibleWords()) {
             if (counts.get(word) == null) {
+                // current word does not exists
                 counts.put(word, 1);
             } else {
-                counts.put(word, counts.get(word) + 1);
+                // current word already exists
+                Integer currValue = counts.get(word);
+                counts.put(word, currValue + 1);
             }
         }
     }
 
     public static void main(String[] args) {
-        //Initialisierung einer Map mit den Worten aus der Bibel
-        Map<String, Integer> counts = new ArrayMap();
-        //Map<String, Integer> counts = new ListMap<>();
-        //Map<String, Integer> counts = new TreeMap<>(Comparator.<String>naturalOrder());
-        countWords(counts);
+        //Map<String, Integer> bibleTreeMap = new TreeMap<>(Comparator.<String>naturalOrder());
+        //countWords(bibleTreeMap);
+        Map<String, Integer> ListMap = new ListMap<>();
+        countWords(ListMap);
 
-        //Initialisierung eines sortierten Arrays mit den Worten
-        String[] words = new String[counts.size()];
-        counts.keys(words);
-        sort(words, counts);
+        // Save words without duplicates
+        String[] setWords = new String[ListMap.size()];
+        ListMap.keys(setWords);
 
+        // Call the method which sorts the array
+        sort(setWords, ListMap);
 
-        // ausgeben aller Worte
-        for (int i = 0; i < words.length; i++){
-            System.out.println(counts.get(words[i]) + " " + words[i]);
+        // Print all words: it costs a lot of time
+        for (String word : setWords) {
+            System.out.println(ListMap.get(word) + " " + word);
         }
     }
 
-    //tauscht zwei benachbarte Elemente
-    static void swap (String[] words, int i){
-        String zwischenspeicher = words[i];
-        words[i] = words[i+1];
-        words[i+1] = zwischenspeicher;
-    }
-
-    //bubble sort = wir vertauschen wiederholt benachbarte Elemente, die falsch herum sortiert sind
-    //siehe Algodat V8-16 ff. Version 17.Juli 2019
     public static void sort(String[] words, Map<String, Integer> counts) {
-        boolean done = false;
-        while (!done){
-            done = true;
-            for (int i = 0; i < words.length-1; i++){
-                if (counts.get(words[i]) > counts.get(words[i+1])){
-                    swap(words, i);
-                    done = false;
+        // We are implementing the BubbleSort
+        String temp;
+        for (int i = 1; i < words.length; i++) {
+            for (int j = 0; j < words.length - i; j++) {
+                if (counts.get(words[j]) >= counts.get(words[j + 1])) {
+                    temp = words[j];
+                    words[j] = words[j + 1];
+                    words[j + 1] = temp;
                 }
             }
         }
