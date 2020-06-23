03ddebb88c9250c917a5c410e07061ad858770b2
Manuela <mkawoung@gmail.com>
2020-06-23 14:48:01 +0200

    WeightedSetCover... wiederherstellt

--- P1/src/test/java/de/tukl/programmierpraktikum2020/p1/a2/WeightedSetCoveringTest.java
+++ P1/src/test/java/de/tukl/programmierpraktikum2020/p1/a2/WeightedSetCoveringTest.java
@@ -2,6 +2,7 @@ package de.tukl.programmierpraktikum2020.p1.a2;
 
 import de.tukl.programmierpraktikum2020.p1.a1.*;
 import de.tukl.programmierpraktikum2020.p1.a1.PriorityQueue;
+import de.tukl.programmierpraktikum2020.p1.a1.PriorityQueueTest;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -10,67 +11,59 @@ import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class WeightedSetCoveringTest {
-    public static List<PriorityQueue<Integer>> getPriorityQueueInstances() {
+    public static List<PriorityQueue<WeightedSet<Integer>>> getPriorityQueueInstances() {
         // Wir nutzen einfach die PriorityQueue Instanzen aus der anderen Testklasse.
-        return PriorityQueueTest.getPriorityQueueInstances();
+
+        List<PriorityQueue<WeightedSet<Integer>>> implementations = new LinkedList<>();
+
+        Comparator<WeightedSet<Integer>> cw;
+        cw = Comparator.comparingDouble(WeightedSet::getWeight);
+
+        implementations.add(new ListQueue<>(cw.reversed()));
+        implementations.add(new SkewHeap<>(cw.reversed()));
+        implementations.add(new FibonacciHeap<>(cw.reversed()));
+        return implementations;
+
     }
 
     @ParameterizedTest
     @MethodSource("getPriorityQueueInstances")
     public void weightedSetBeispiel(PriorityQueue<WeightedSet<Integer>> queue) {
+
         System.out.println("Teste weightedSetBeispiel mit " + queue.getClass().getSimpleName());
 
-        //fail("Test wurde noch nicht implementiert");
-        Comparator<WeightedSet<Integer>> cw;
-        cw = Comparator.comparing(WeightedSet::getWeight);
-        if (queue instanceof ListQueue) {
-            System.out.println("It is a ListQueue");
-            queue = new ListQueue<>(cw.reversed());
-        } else if (queue instanceof SkewHeap) {
-            queue = new SkewHeap<>(cw.reversed());
-            System.out.println("It is a SkewHeap");
-        } else {
-            queue = new FibonacciHeap<>(cw.reversed());
-            System.out.println("It is a FibonacciHeap");
+        Bundle bundle1 = new Bundle("bundle 1", 1.0, Set.of(1, 2));
+        Bundle bundle2 = new Bundle("bundle 2", 2.0, Set.of(2, 3));
+        Bundle bundle3 = new Bundle("bundle 3", 10.0, Set.of(2, 3, 5));
+        Bundle bundle4 = new Bundle("bundle 4", 3.0, Set.of(4));
+        Bundle bundle5 = new Bundle("bundle 5", 4.0, Set.of(1, 5));
+        Bundle bundle6 = new Bundle("bundle 6", 10.0, Set.of(2, 3, 4));
+
+        Set<WeightedSet<Integer>> familyOfSets = Set.of(bundle1, bundle2, bundle3, bundle4, bundle5, bundle6);
+        Set<Integer> targetSet = Set.of(1, 2, 3, 4, 5);
+
+        WeightedSetCovering<Integer> test = new WeightedSetCovering<>(targetSet, familyOfSets, queue);
+        Set<WeightedSet<Integer>> result = test.greedyWeightedCover();
+        Set<String> soll_es_sein = Set.of("Bundle bundle 5", "Bundle bundle 2", "Bundle bundle 1", "Bundle bundle 4");
+
+        Set<Integer> erwartet = Set.of(1, 2, 3, 4, 5);
+        Set<Integer> features = new HashSet<>();
+        for (WeightedSet<Integer> w : result) {
+            features.addAll(w.getSet());
         }
-        assertTrue(queue.isEmpty());
-
-        //or
-        //Set<Integer> s1 = new HashSet<>();
-        //s1.add(1); //..........
-        //Bundle bundle1 = new Bundle("Bundle 1", 1, s1);
-
-        //or--------------------------------------------------------------------------------
-        Bundle bundle1 = new Bundle("Bundle 1", 1, new HashSet<>(Arrays.asList(1, 2)));
-        Bundle bundle2 = new Bundle("Bundle 2", 2, new HashSet<>(Arrays.asList(1, 3)));
-        Bundle bundle3 = new Bundle("Bundle 3", 10, new HashSet<>(Arrays.asList(2, 3, 5)));
-        Bundle bundle4 = new Bundle("Bundle 4", 3, new HashSet<>(Collections.singletonList(4)));
-        Bundle bundle5 = new Bundle("Bundle 5", 4, new HashSet<>(Arrays.asList(1, 5)));
-        Bundle bundle6 = new Bundle("Bundle 6", 10, new HashSet<>(Arrays.asList(2, 3, 4)));
-
-        Set<Integer> targetSet = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5));
-        Set<WeightedSet<Integer>> familyOfSets = new HashSet<>(Arrays.asList(bundle1, bundle2, bundle3, bundle4, bundle5, bundle6));
-        WeightedSetCovering<Integer> weightedSetCovering = new WeightedSetCovering<>(targetSet, familyOfSets, queue);
-        Set<WeightedSet<Integer>> greedySolution = weightedSetCovering.greedyWeightedCover();
-
-        double totalCost = 0;
-        for (WeightedSet<Integer> weightedSet : greedySolution) {
-            totalCost += weightedSet.getWeight() * weightedSet.getSet().size();
+        double price = 0.0;
+        for (WeightedSet<Integer> set : result) {
+            Bundle bundle = (Bundle) set;
+            price = price + bundle.price;
         }
-        assertEquals(10, totalCost);
-        //prüfe ob den greedy alle bundle enthält
-        assertTrue(greedySolution.contains(bundle1));
-        assertTrue(greedySolution.contains(bundle2));
-        assertFalse(greedySolution.contains(bundle3));
-        assertTrue(greedySolution.contains(bundle4));
-        assertTrue(greedySolution.contains(bundle5));
-        assertFalse(greedySolution.contains(bundle6));
-        assertFalse(queue.isEmpty());
-=======
 
-        assertEquals(10.0,price,"die Kosten betragen nicht 10€");
-        assertEquals(erwartet,features,"es sind nicht alle Features enthalten");
-        assertEquals(soll_es_sein, bundles,"die Bundels entsprechen nicht denen aus dem Bsp");
->>>>>>> fe184471723d3b66943356e4821c66619ca99c9e
+        Set<String> bundles = new HashSet<>();
+        for (WeightedSet<Integer> w : result) {
+            bundles.add(w.toString());
+        }
+        assertEquals(10.0, price, "die Kosten betragen nicht 10€");
+        assertEquals(erwartet, features, "es sind nicht alle Features enthalten");
+        assertEquals(soll_es_sein, bundles, "die Bundels entsprechen nicht denen aus dem Bsp");
     }
+
 }
